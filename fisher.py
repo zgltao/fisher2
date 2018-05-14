4e3ea921e350f19de5cb8e310e8b24b0633a1bcd
Macx <123>
2018-05-15 01:10:16 +0800

    1

--- fisher.py
+++ fisher.py
@@ -1,3 +1,14 @@
 from flask import Flask
 
 
+app = Flask(__name__)
+app.config.from_object('config')
+
+@app.route('/hello')
+def hello():
+    return 'hello flask'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=app.config['DEBUG'], port=8111)
+
